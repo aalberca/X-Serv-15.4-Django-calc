68216d26343ac02e093b3f1bdafe235a5b14800f
Ana Alberca Diaz Plaza <AnaAlberca@MacBook-Pro-de-Ana.local>
2016-03-21 19:44:00 +0100

    Entrega ejercicio

--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'(?P<numero1>\d+)\+(?P<numero2>\d+)', 'calc.views.suma'),
+    url(r'(?P<numero1>\d+)-(?P<numero2>\d+)', 'calc.views.resta'),
+    url(r'(?P<numero1>\d+)\*(?P<numero2>\d+)', 'calc.views.multip'),
+    url(r'(?P<numero1>\d+)\/(?P<numero2>\d+)', 'calc.views.division'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'.*', 'calc.views.error'),
 )
